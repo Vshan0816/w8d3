99e003e877e44ccbe744431b785440351408d452
Vincent <vshan0816@gmail.com>
2023-08-16 17:01:20 -0400

    Finish absurdBubbleSort

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdBubbleSort.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdBubbleSort.js
@@ -10,7 +10,7 @@ function askifGreaterThan(el1, el2, callback) {
 
     reader.question(
 
-        "Is " + el1 + " greater than " + el2 + "?", response => {
+        "Is " + el1 + " greater than " + el2 + "?: ", response => {
 
             if (response === 'yes') {
                 callback(true);
@@ -22,13 +22,14 @@ function askifGreaterThan(el1, el2, callback) {
 };
 
 
-function callback(bool) {
-    reader.close();
-    return bool;
+// function callback(bool) {
+//     reader.close();
+//     return bool;
     
-}
+// }
 
-
+// ask if true should change boolean of made any swaps
+// identify whether array at i is greater than array of i+1
 
 function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
 
@@ -46,17 +47,40 @@ function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
                 arr[i+1] = temp;
                 madeAnySwaps = true;
             }
+        innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
         });
 
-        innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop)
+        
     }; //end if
 
 }
 
-function outerBubbleSortLoop() {
-    console.log("In outer bubble sort")
-}
+// function outerBubbleSortLoop() {
+//     console.log("In outer bubble sort")
+// }
 
 
-// askifGreaterThan(1, 2, callback);
-innerBubbleSortLoop([3,2,1], 0, false, outerBubbleSortLoop)
+// // askifGreaterThan(1, 2, callback);
+// innerBubbleSortLoop([3,2,1], 0, false, outerBubbleSortLoop)
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+    function outerBubbleSortLoop(madeAnySwaps) {
+        if (madeAnySwaps) {
+            innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+        } else {
+            sortCompletionCallback(arr)
+        }
+      // Begin an inner loop if you made any swaps. Otherwise, call
+      // `sortCompletionCallback`.
+    }
+  
+    // Kick the first outer loop off, starting `madeAnySwaps` as true.
+    outerBubbleSortLoop(true)
+  }
+
+  absurdBubbleSort([3, 2, 1], function(arr) {
+    console.log("Sorted array: " + JSON.stringify(arr));
+    reader.close();
+  });
+
+
